ee356e828e4d680dea947627e7b8f34960130e3c
EagleCyber-74 <afdalreal2022@gmail.com>
2024-08-25 18:05:24 +0700

    Locator

---
+++ locator.sh
@@ -0,0 +1,48 @@
+#JAGAN DI RECODE BREE
+#GITHUB :https://github.com/EagleCyber-74
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+
+
+echo $ku "[$pu+$ku]$nama sampean sopo mas "
+nama mu 
+sleep 0.01
+echo $ku"Loading......!!"
+sleep 3
+echo " 
+.                         ⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⡿⠿⠿⠿⠿⢿⣿⣿⣷⣦⣄⣀⣤⣶⣶
+⠀⠀⠀⠀⠀⣰⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣿⣿⠟⠋
+⠀⠀⠀⠀⣼⣿⡿⠃⠀⢀⣤⣾⣿⣿⣿⣿⣷⣦⣄⠀⠀⠈⠉⠀⠀⠀
+⠀⠀⠀⣸⣿⡿⠁⠀⢠⣿⣿⠟⠉⠀⠈⠉⠛⢿⣿⣷⡄⠀⠀⠀⠀⠀
+⠀⠀⢀⣿⣿⡇⠀⠀⣾⣿⡟⠀⠀⢀⣤⣄⠀⠀⠹⣿⣿⡄⠀⠀⠀⠀
+⠀⠀⣾⣿⣿⡇⠀⠀⢻⣿⣷⡀⠀⠘⣿⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀
+⠀⣼⣿⡿⣿⣿⡄⠀⠈⠻⣿⣿⣷⣿⣿⡿⠃⠀⢀⣿⣿⡇⠀⠀⠀⠀
+⣰⣿⣿⠁⠹⣿⣿⣦⡀⠀⠈⠉⠛⠋⠉⠀⠀⣠⣾⣿⡟⠀⠀⠀⠀⠀
+⣿⣿⣧⣤⣤⣬⣿⣿⣿⣶⣦⣤⣤⣤⣴⣶⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀
+⠙⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠛⠋⠁            "
+echo 
+echo $or"-------------------------------------------------------------------------------------"
+sleep 0.03
+echo $me"°°         Author : EagleCyber         °°"
+sleep 0.03
+echo $ji"##  https://github.com/EagleCyber-74   ##"
+sleep 0.03
+echo $or"--------------------------------------------------------------------------------------"
+
+read -p "Masukkan Ip andres:" Ip
+curl -s http://ip-api.com/$Ip
+
+echo $me"•••••••••••••••••••••••••••••••"
+echo $ku"=   Terimakasih    ="
+echo $cy"=     Telah        ="
+echo $ij"=  Menggunakan     ="
+echo $bi"=    $nama         ="
+echo $pr"•••••••••••••••••••••••••••••••••"
